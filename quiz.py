b8ae97cdb35d6e04e0396492bef8fed67d280e7b
Khalil Abdallah Melab <avjit1963@gmail.com>
2026-06-22 20:14:38 +0400

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,88 @@
+print("🌏1. World")
+print("What was the Sahara Desert like in the past :")
+print("A) it was always a desert")
+print("B) it was underwater")
+print("C) it was green with lakes and animals")
+print("D) it never changed")
+rs=input("Choose the correct answer (A/B/C/D)")
+if rs=="A":
+   print("Incorrect answer😉 C is the right answer")
+elif rs =="B":
+   print("Incorrect answer😉 C is the right answer")
+elif rs =="C":
+   print("Correct🎉!")
+elif rs=="D":
+   print("Incorrect answer😉 C is the right answer")
+
+
+
+
+print("🧠2. Brain")
+print("How many thoughts does the human brain produce per day(approx) :")
+print("A) 1,000")
+print("B) 10,000")
+print("C) 60,000")
+print("D) 1,000,000")
+ds=input("Choose the correct answer (A/B/C/D) :")
+if ds=="A":
+   print("Incorrect answer😉 C is the right answer")
+elif ds=="B":
+   print("Incorrect answer😉 C is the right answer")
+elif ds=="C":
+   print("Correct🎉!")
+elif ds=="D":
+   print("Incorrect answer😉 C is the right answer")
+
+
+
+print("🐙3.Animals")
+
+print("How many hearts does an octopus have? ")
+
+print("A) 1")
+print("B) 2")
+print("C) 3")
+print("D) 4")
+
+ls=input("Choose the correct answer (A/B/C/D) :")
+if ls=="A":
+   print("Incorrect answer😉 C is the right answer")
+elif ls=="B":
+   print("Incorrect answer😉 C is the right answer")
+elif ls=="C":
+   print("Correct🎉!")
+elif ls=="D":
+   print("Incorrect answer😉 C is the right answer")
+
+print("🏛️4.History")
+print("Who is closer to us in time?")
+print("A) The Pyramids of Giza")
+print("B) Cleopatra")
+print("C) Dinosaurs")
+print("D) Early Humans")
+vs=input("Choose the correct answer (A/B/C/D) :")
+if vs=="A":
+   print("Incorrect answer😉 B is the right answer")
+elif vs=="B":
+   print("Correct answer🎉!")
+elif vs=="C":
+   print("Incorrect answer😉 B is the right answer")
+elif vs=="D":
+   print("Incorrect answer😉 B is the right answer")
+print("🌌5.Space")
+print("Which planet has a day longer than its year?")
+print("A) Mars")
+print("B) Jupiter")
+print("C) Saturn")
+print("D) Venus")
+ps=input("Choose the correct answer (A/B/C/D) :")
+if ps=="A":
+   print("Incorrect answer😉 D is the right answer")
+elif ps=="B":
+   print("Incorrect answer😉 D is the right answer")
+elif ps=="C":
+   print("Incorrect answer😉 D is the right answer")
+elif ps=="D":
+   print("Correct🎉!")
+
+
